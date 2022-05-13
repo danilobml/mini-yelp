1eba7fa8e7c70df76a0d6b744d22a30f3057b626
danilobml <danbarml@gmail.com>
2022-05-13 18:21:06 +0200

    styling restaurant component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,16 +1,29 @@
+import { Container, Row, Col, Button, FormSelect, FormControl, InputGroup, Form } from "react-bootstrap";
+
 const Search = ({ handleFilter, handleSearch, handleUserInput }) => {
   return (
-    <div>
-      <select name="filter" id="filter" onInput={(e) => handleFilter(e)}>
-        <option value="restaurant">Name</option>
-        <option value="categories">Type</option>
-        <option value="city">City</option>
-      </select>
-      <form onSubmit={(e) => handleSearch(e)}>
-        <input type="text" onChange={(e) => handleUserInput(e)} />
-        <input type="submit" value="search" />
-      </form>
-    </div>
+    <Container>
+      <Row>
+        <Col sm={2} className="me-0">
+          <FormSelect className="me-0" name="filter" id="filter" onInput={(e) => handleFilter(e)}>
+            <option>Filter by:</option>
+            <option value="restaurant">Name</option>
+            <option value="categories">Type</option>
+            <option value="city">City</option>
+          </FormSelect>
+        </Col>
+        <Col sm={8} className="ms-0">
+          <Form className="ms-0" onSubmit={(e) => handleSearch(e)}>
+            <InputGroup>
+              <FormControl placeholder="Search" onChange={(e) => handleUserInput(e)} />
+              <Button variant="primary" type="submit">
+                Search
+              </Button>
+            </InputGroup>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 export default Search;
